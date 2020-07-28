39d50b0ef3f2906a42c50bdd7a0a8c45ef9a6fb2
AndreCastro <andreluizcarneiro@outlook.com.br>
2020-07-28 15:47:37 -0300

    Modulo EsNext

---
+++ set.js
@@ -0,0 +1,18 @@
+// não aceita repetição - não indexada
+const times = new Set()
+times.add('Vasco')
+times.add('Sao Paulo').add('Palmeiras').add('Corinthians')
+times.add('Vasco')
+
+console.log(times)
+console.log(times.size)
+console.log(times.has('vasco'))
+console.log(times.has('Vasco'))
+times.delete('Palmeiras')
+console.log(times.has('Flamengo'))
+
+const nomes = ['Raquel', 'Lucas', 'Julia', 'Lucas']
+const nomesSet = new Set(nomes)
+console.log(nomesSet)
+
+
